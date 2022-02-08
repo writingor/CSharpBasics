39cef88fd58b4c557e03ebfc67901f03d792c85b
Igor Rasskazov <writingornet@gmail.com>
2022-02-08 12:01:18 -0800

    Introduction to boolean data type.

---
+++ bool/bool/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// boolean type
+
+bool yes = true;
+bool no = false;
+
+Console.WriteLine(5 == 6); // false
+Console.WriteLine(5 == 5); // true
+
+Console.WriteLine(5 < 6);
+Console.WriteLine(5 > 6);
+Console.WriteLine(5 <= 6);
+
+// with strings don't work < >
+
+// but work with ==
+
+bool strings = "100" == "100";
+Console.WriteLine(strings);
+
