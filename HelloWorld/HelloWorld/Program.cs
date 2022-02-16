d8743ec83214b5aa107dbe5c434ed85fb41f538c
Igor Rasskazov <writingornet@gmail.com>
2022-02-16 12:48:35 -0800

    Create Hellow World.

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello, World!");
+            }
+            finally
+            {
+                Console.Beep();
+
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("Press any key to close the program:");
+                Console.ReadKey();
+            }
+        }
+    }
+}
