3a188b10b10fda6341172bf6d194191dc9293dd2
Igor Rasskazov <writingornet@gmail.com>
2022-02-19 10:33:13 -0800

    Some Math

---
+++ Math/Math/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+byte b1, b2;
+b1 = 10; b2 = 2;
+
+
+// %
+
+int remOfB1AndB2 = b1 % b2;
+Console.WriteLine(remOfB1AndB2);
+
+
+// Math.Pow()
+
+double mathPow = Math.Pow(b1, b2);
+Console.WriteLine(mathPow);
+
+
+// Math.Sqrt()
+
+double mathSqrt = Math.Sqrt(b1);
+Console.WriteLine(mathSqrt);
+
+
+
+
+
