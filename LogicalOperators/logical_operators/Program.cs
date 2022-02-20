2d69bf64bb5d1585d3e6ab73e8a85a6be3b6a7f6
Igor Rasskazov <writingornet@gmail.com>
2022-02-20 08:30:07 -0800

    Added ternary operators.

--- LogicalOperators/logical_operators/Program.cs
+++ LogicalOperators/logical_operators/Program.cs
@@ -3,48 +3,92 @@
 // if else
 
 if (22 == 24)
-{
     Console.WriteLine("24 as 24.");
-}
 else
-{
     Console.WriteLine("Something broke.");
-}
+
+
+// if else if
 
 int i = -1;
 
 if (i < 0)
-{
     Console.WriteLine($"The variable i = {i}. This is less than 0.");
-}
 else if (i < 5)
-{
     Console.WriteLine($"The variable i = {i}. This is less than 5.");
-}
 else if (i == 10)
-{
     Console.WriteLine($"The variable i = {i}. This is equal to 10.");
-}
 else
-{
     Console.WriteLine($"The variable i = {i}. This is more than 10.");
-}
+
+
+// () ? :
+
+// Be sure to return the result!!!
+
+byte b1 = 1, b2 = 2; 
+
+byte resultOfTernaryOperatorOne = (b1 > b2) ? b1 : b2;
+bool resultOfTernaryOperatorTwo = (b1 > b2) ? false : true;
+
+Console.WriteLine($"resultOfTernaryOperatorOne = {resultOfTernaryOperatorOne}");
+Console.WriteLine($"resultOfTernaryOperatorTwo = {resultOfTernaryOperatorTwo}");
+
+
+// += ? :
+
+string string1 = "DefaultValue ";
+string1 += b1 == 1 ? "Value1" : "Value2";
+
+Console.WriteLine(string1);
+
+
+// = ? :
+
+string string2 = "DefaultValue ";
+string2 = b1 == 2 ? "Value1" : "Value2";
+
+Console.WriteLine(string2);
+
+
+// Nested ternary operators. Too hard to read...
+
+bool bool1 = true;
+byte byte1 = 1;
+
+string string3 = (bool1 == true) ? ((byte1 == 0) ? "Meow" : "Wof") : ((byte1 == 1) ? "Moo" : "Blew");
+Console.WriteLine(string3);
+
+string3 = bool1 == false ? (byte1 == 1 ? "Meow" : "Wof") : (byte1 == 0 ? "Moo" : "Blew");
+Console.WriteLine(string3);
+
 
 // switch
 
-switch (i)
+int switcher = 6;
+
+switch (switcher)
 {
-    case 0:
-        Console.WriteLine(0);
+    case 0: // string "", char ''
+        Console.WriteLine("Zero.");
         break;
-    case 5:
-        Console.WriteLine(5);
+    case 1:
+    case 2:
+    case 3:
+    case 5: // string "", char ''
+        Console.WriteLine("Less than six. But not four or zero!");
         break;
+    case 6:
+        Console.WriteLine("Oh no! I hate the number six! I'll turn her into...");
+        goto case 0;
     default:
-        Console.WriteLine("Something other.");
+        Console.WriteLine(switcher);
         break;
 }
 
+
+
+#region Who are you?
 
 Console.Write("Enter your name: ");
 string name = Console.ReadLine();
@@ -59,8 +103,10 @@ switch (name)
         break;
 }
 
+#endregion
 
-// || && !
+
+# region Game
 
 int money = 100;
 int minPrice = 20;
@@ -72,34 +118,26 @@ int price = int.Parse(Console.ReadLine());
 // AND &&
 
 if (minPrice <= price && price <= money)
-{
     Console.WriteLine("Buying!");
-}
 else
-{
     Console.WriteLine("No thanks!");
-}
+
 
 // OR ||
 
 if (minPrice > price || price > money)
-{
     Console.WriteLine("No thanks!");
-}
 else
-{
     Console.WriteLine("Buying!");
-}
+
 
 // NOT !
 
 if (!(minPrice > price || price > money))
-{
     Console.WriteLine("Buying!");
-}
 else
-{
     Console.WriteLine("No thanks!");
-}
 
+Console.WriteLine("Tnx!");
 
+# endregion
