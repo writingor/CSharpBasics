c3c87b8259ec26d4354e9b7442dbd68d8bffd0a2
Igor <98356121+writingor@users.noreply.github.com>
2022-02-21 11:22:10 -0800

    clear

--- Constant/Constant/Program.cs
+++ Constant/Constant/Program.cs
@@ -18,25 +18,6 @@ int toIntCasting = (int) absoluteZero;
 
 Console.WriteLine(toIntCasting);
 
-// 256 toByteCasting = 0, 257 = 1, 267 = 11... :)
-// 256 is  0000 0000  0000 0000  0000 0001  0000 0000 and byte is 0000 0000
-// 257 is  0000 0000  0000 0000  0000 0001  0000 0001 and byte is 0000 0001
-// 267 is  0000 0000  0000 0000  0000 0001  0000 1011 and byte is 0000 1011
-
-// -256 toByteCasting = 0, 257 = 255, 267 = 245... :D
-// 256 is  1000 0000  0000 0000  0000 0001  0000 0000 and byte is 0000 0000
-// 257 is  1000 0000  0000 0000  0000 0001  0000 0001 and byte is ... 1111 1111 WHY?
-// 267 is  1000 0000  0000 0000  0000 0001  0000 1011 and byte is ... 1111 0101 WHY?
-// BECAUSE IN SIGNED DATA TYPE FIRST BIT FROM LEFT IS PLUS (0) OR MINUS (1)
-// IN BYTE 255 is 1111 1111, IN SBYTE -1 is 1111 1111 
-// 0111 1111 is 127
-// 1111 1111 is -128
-//
-// 0000 0011 is 3
-// use NOT ~
-// 1111 1100 is -4
-// sum 1 (0000 0001)
-// result: 1111 1101 is -3
 
 byte toByteCasting = (byte) toIntCasting;
 
