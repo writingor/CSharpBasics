be91c9c0135f1d2ba99519789e22e895a3e6bb2a
Igor <98356121+writingor@users.noreply.github.com>
2022-02-25 01:29:52 -0800

    Loop through the array and assign a value.

--- NDimmensionArray/NDimmensionArray/Program.cs
+++ NDimmensionArray/NDimmensionArray/Program.cs
@@ -4,29 +4,31 @@ byte[] exampleArray = new byte[100]; // big array
 byte[] oneDimmensionArray = { 1, 2, 3 }; // == byte[] oneDimmensionArray = new byte[] { 1, 2, 3 };
 
 
-byte[,] twoDimmensionArray = new byte[3, 3]
+int[,] twoDimmensionArray = new int[3, 3]
 {
     {1, 2, 3},
     {4, 5, 6},
     {7, 8, 9},
 };
 
+int num = 1;
+
+for (int i = 0; i < 3; i++)
+{
+    for (int n = 0; n < 3; n++)
+    {
+        twoDimmensionArray[i, n] = num;
+        num++;
+    }
+}
+
 for (int i = 0; i < 3; i++)  // i < twoDimmensionArray.GetLength(i) not have more array
 {
-    for (int n = 0; n < 3; n++)  //n <  twoDimmensionArray.GetLength(n) not have more array
+    for (int n = 0; n < 3; n++)  
     {
         Console.Write("{0} ", twoDimmensionArray[i, n]);
-
-        //if (n == twoDimmensionArray.GetLength(n))
-        //{
-        //    break;
-        //}
     }
     Console.WriteLine();
-    //if(i == twoDimmensionArray.GetLength(i))
-    //{
-    //    break;
-    //}
 }
 
 
