1151d5788684ea13c7ae6c857282a1ea32cf5818
Igor <98356121+writingor@users.noreply.github.com>
2022-02-24 23:07:02 -0800

    loop in arrays

--- NDimmensionArray/NDimmensionArray/Program.cs
+++ NDimmensionArray/NDimmensionArray/Program.cs
@@ -1,6 +1,7 @@
 ﻿
+byte[] exampleArray = new byte[100]; // big array
 
-byte[] oneDimmensionArray = { 1, 2, 3 };
+byte[] oneDimmensionArray = { 1, 2, 3 }; // == byte[] oneDimmensionArray = new byte[] { 1, 2, 3 };
 
 
 byte[,] twoDimmensionArray = new byte[3, 3]
@@ -10,6 +11,24 @@ byte[,] twoDimmensionArray = new byte[3, 3]
     {7, 8, 9},
 };
 
+for (int i = 0; i < 3; i++)  // i < twoDimmensionArray.GetLength(i) not have more array
+{
+    for (int n = 0; n < 3; n++)  //n <  twoDimmensionArray.GetLength(n) not have more array
+    {
+        Console.Write("{0} ", twoDimmensionArray[i, n]);
+
+        //if (n == twoDimmensionArray.GetLength(n))
+        //{
+        //    break;
+        //}
+    }
+    Console.WriteLine();
+    //if(i == twoDimmensionArray.GetLength(i))
+    //{
+    //    break;
+    //}
+}
+
 
 byte[,,] threeDimmensionArray = new byte[3, 3, 3];
 
@@ -18,7 +37,7 @@ threeDimmensionArray[0, 0, 1] = 2;
 threeDimmensionArray[1, 1, 1] = 3;
 
 
-# region Calendar
+# region Calendar (Jagged Array)
 
 byte[][] months = new byte[12][];
 // array in array
